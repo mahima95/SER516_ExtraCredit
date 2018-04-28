f7a3da04b3ab6412cc25ae8034c9c4d8452ff5c1
Mahima Gupta <mahimagupta1195@gmail.com>
2018-04-27 20:37:37 -0700

    Clean design for detetection panel

--- Project3-WebApp/src/main/java/server/view/DetectionPanel.java
+++ Project3-WebApp/src/main/java/server/view/DetectionPanel.java
@@ -23,7 +23,7 @@ import server.controller.DetectionController;
 
 /**
  * View for the Detection Panel.
- * @SER516 Project3_Team03
+ * @SER516 SER516_ExtraCredit
  * @Version 1.0
  */
 @SuppressWarnings("serial")
@@ -31,20 +31,23 @@ public class DetectionPanel extends JPanel {
 
 	public JTextField timeElapsedTextbox;
 	public JSpinner spinnerEmoStateInterval, spinnerUpperFace, spinnerLowerFace, spinnerAffective;
-	public JCheckBox chckbxEyeAutoReset;
-	public JTextArea txtAreaEmoLogs;
+	public JCheckBox checkboxEyeAutoReset;
+	public JTextArea textAreaEmoLogs;
 	public JScrollPane scroll;
-	public JButton btnSend, btnClearLogs;
+	public JButton buttonSend, buttonClearLogs;
 	@SuppressWarnings("rawtypes")
 	public JComboBox comboUpperFace, comboLowerFace, comboEye, comboAffective;
-	public JRadioButton rdbtnActive;
+	public JRadioButton radiobuttonActive;
 	public FaceExpressionData faceExpressionData;
 	public FaceAffectiveData faceAffectiveData;
-	public JLabel lblEmoState,lblTime,lblSeconds,lblUpperFace,lblEye,lblLowerface,lblAffective,lblEmoengineLogs;
+	public JLabel labelEmoState,labelTime,labelSeconds,labelUpperFace,labelEye,labelLowerface,labelAffective,labelEmoengineLogs;
 	public SpringLayout springpanel;
 	public ServerConsole serverconsole;
 	public DetectionController detectionController=new DetectionController();
 
+	/**
+	 * Constructor of detetction Panel
+	 */
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public DetectionPanel() {
 		
@@ -53,18 +56,18 @@ public class DetectionPanel extends JPanel {
 
 		faceAffectiveData = new FaceAffectiveData();
 		faceExpressionData = new FaceExpressionData();
-		lblEmoState = new JLabel("EMOTION STATE");
-		lblTime = new JLabel("TIME: ");
-		lblSeconds = new JLabel("Seconds");
-		lblUpperFace = new JLabel("Upperface:");
-		lblLowerface = new JLabel("Lowerface:");
+		labelEmoState = new JLabel("EMOTION STATE");
+		labelTime = new JLabel("TIME: ");
+		labelSeconds = new JLabel("Seconds");
+		labelUpperFace = new JLabel("Upperface:");
+		labelLowerface = new JLabel("Lowerface:");
 		comboLowerFace = new JComboBox();
-		rdbtnActive = new JRadioButton("Active");
-		chckbxEyeAutoReset = new JCheckBox("Auto Reset");
-		lblEmoengineLogs = new JLabel("EmoEngine Logs");
-		btnClearLogs = new JButton("Clear Logs");
-		lblAffective = new JLabel("Affective:");
-		lblEye = new JLabel("Eye:");
+		radiobuttonActive = new JRadioButton("Active");
+		checkboxEyeAutoReset = new JCheckBox("Auto Reset");
+		labelEmoengineLogs = new JLabel("EmoEngine Logs");
+		buttonClearLogs = new JButton("Clear Logs");
+		labelAffective = new JLabel("Affective:");
+		labelEye = new JLabel("Eye:");
 		
 		
 		timeElapsedTextbox = new JTextField();
@@ -90,9 +93,9 @@ public class DetectionPanel extends JPanel {
 		comboEye.setModel(new DefaultComboBoxModel(
 				new String[] { "Blink", "Wink Left", "Wink Right", "Look Left", "Look Right" }));
 		
-		txtAreaEmoLogs = new JTextArea();
-		txtAreaEmoLogs.setEditable(true);
-		txtAreaEmoLogs.setRows(5);
+		textAreaEmoLogs = new JTextArea();
+		textAreaEmoLogs.setEditable(true);
+		textAreaEmoLogs.setRows(5);
 		
 		comboAffective = new JComboBox();
 		comboAffective.setModel(new DefaultComboBoxModel(new String[] { "Meditation", "Engagement Boredom",
@@ -103,9 +106,9 @@ public class DetectionPanel extends JPanel {
 				new String[] { "0", "0.1", "0.2", "0.3", "0.4", "0.5", "0.6", "0.7", "0.8", "0.9", "1.0" }));
 		
 		serverconsole = ServerConsole.getInstance();
-		serverconsole.setJtextArea(txtAreaEmoLogs);
+		serverconsole.setJtextArea(textAreaEmoLogs);
 				
-		scroll = new JScrollPane(txtAreaEmoLogs);
+		scroll = new JScrollPane(textAreaEmoLogs);
 		serverconsole.setJScrollPane(scroll);
 		
 		setComponentLayout();
@@ -114,35 +117,39 @@ public class DetectionPanel extends JPanel {
 		
 		 detectionController = detectionController.new DetectionControllerBuilder( spinnerUpperFace,  spinnerLowerFace,
 				spinnerAffective,  comboUpperFace, comboLowerFace,
-					comboAffective, comboEye,  chckbxEyeAutoReset,
-					rdbtnActive,txtAreaEmoLogs, btnClearLogs,  faceAffectiveData,
+					comboAffective, comboEye,  checkboxEyeAutoReset,
+					radiobuttonActive,textAreaEmoLogs, buttonClearLogs,  faceAffectiveData,
 					 faceExpressionData).createDetectionControl();
 		
 		
 	}
 	
+	/*
+	 * Layout of server components
+	 */
+	
 	public void setComponentLayout(){
 		springpanel.putConstraint(SpringLayout.NORTH, comboLowerFace, 144, SpringLayout.NORTH, this);
 		
-		springpanel.putConstraint(SpringLayout.NORTH, lblEmoState, 13, SpringLayout.NORTH, this);
-		springpanel.putConstraint(SpringLayout.WEST, lblEmoState, 12, SpringLayout.WEST, this);
-		springpanel.putConstraint(SpringLayout.EAST, lblEmoState, 88, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.NORTH, labelEmoState, 13, SpringLayout.NORTH, this);
+		springpanel.putConstraint(SpringLayout.WEST, labelEmoState, 12, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.EAST, labelEmoState, 88, SpringLayout.WEST, this);
 
-		springpanel.putConstraint(SpringLayout.NORTH, lblTime, 47, SpringLayout.NORTH, this);
-		springpanel.putConstraint(SpringLayout.WEST, lblTime, 22, SpringLayout.WEST, this);
-		springpanel.putConstraint(SpringLayout.EAST, lblTime, 70, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.NORTH, labelTime, 47, SpringLayout.NORTH, this);
+		springpanel.putConstraint(SpringLayout.WEST, labelTime, 22, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.EAST, labelTime, 70, SpringLayout.WEST, this);
 		
 		springpanel.putConstraint(SpringLayout.NORTH, timeElapsedTextbox, 44, SpringLayout.NORTH, this);
 		springpanel.putConstraint(SpringLayout.WEST, timeElapsedTextbox, 74, SpringLayout.WEST, this);
 		
-		springpanel.putConstraint(SpringLayout.NORTH, lblSeconds, 47, SpringLayout.NORTH, this);
-		springpanel.putConstraint(SpringLayout.WEST, lblSeconds, 202, SpringLayout.WEST, this);
-		springpanel.putConstraint(SpringLayout.EAST, lblSeconds, 258, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.NORTH, labelSeconds, 47, SpringLayout.NORTH, this);
+		springpanel.putConstraint(SpringLayout.WEST, labelSeconds, 202, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.EAST, labelSeconds, 258, SpringLayout.WEST, this);
 		
-		springpanel.putConstraint(SpringLayout.WEST, lblUpperFace, 0, SpringLayout.WEST, lblEmoState);
-		springpanel.putConstraint(SpringLayout.EAST, lblUpperFace, 88, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.WEST, labelUpperFace, 0, SpringLayout.WEST, labelEmoState);
+		springpanel.putConstraint(SpringLayout.EAST, labelUpperFace, 88, SpringLayout.WEST, this);
 		
-		springpanel.putConstraint(SpringLayout.SOUTH, lblUpperFace, -6, SpringLayout.NORTH, comboUpperFace);
+		springpanel.putConstraint(SpringLayout.SOUTH, labelUpperFace, -6, SpringLayout.NORTH, comboUpperFace);
 		springpanel.putConstraint(SpringLayout.NORTH, comboUpperFace, 144, SpringLayout.NORTH, this);
 		springpanel.putConstraint(SpringLayout.WEST, comboUpperFace, 12, SpringLayout.WEST, this);
 		springpanel.putConstraint(SpringLayout.EAST, comboUpperFace, 111, SpringLayout.WEST, this);
@@ -151,82 +158,90 @@ public class DetectionPanel extends JPanel {
 		springpanel.putConstraint(SpringLayout.WEST, spinnerUpperFace, 125, SpringLayout.WEST, this);
 		springpanel.putConstraint(SpringLayout.EAST, spinnerUpperFace, 190, SpringLayout.WEST, this);
 		
-		springpanel.putConstraint(SpringLayout.NORTH, lblLowerface, 0, SpringLayout.NORTH, lblUpperFace);
-		springpanel.putConstraint(SpringLayout.WEST, lblLowerface, 276, SpringLayout.WEST, this);
-		springpanel.putConstraint(SpringLayout.EAST, lblLowerface, -126, SpringLayout.EAST, this);
+		springpanel.putConstraint(SpringLayout.NORTH, labelLowerface, 0, SpringLayout.NORTH, labelUpperFace);
+		springpanel.putConstraint(SpringLayout.WEST, labelLowerface, 276, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.EAST, labelLowerface, -126, SpringLayout.EAST, this);
 	
 		springpanel.putConstraint(SpringLayout.EAST, comboLowerFace, -10, SpringLayout.WEST, spinnerLowerFace);
 		springpanel.putConstraint(SpringLayout.NORTH, spinnerLowerFace, 144, SpringLayout.NORTH, this);
 		springpanel.putConstraint(SpringLayout.WEST, spinnerLowerFace, 402, SpringLayout.WEST, this);
 		springpanel.putConstraint(SpringLayout.EAST, spinnerLowerFace, 458, SpringLayout.WEST, this);
 		
-		springpanel.putConstraint(SpringLayout.WEST, lblEye, 0, SpringLayout.WEST, lblEmoState);
-		springpanel.putConstraint(SpringLayout.EAST, lblEye, -2, SpringLayout.EAST, lblTime);
+		springpanel.putConstraint(SpringLayout.WEST, labelEye, 0, SpringLayout.WEST, labelEmoState);
+		springpanel.putConstraint(SpringLayout.EAST, labelEye, -2, SpringLayout.EAST, labelTime);
 		
-		springpanel.putConstraint(SpringLayout.SOUTH, lblEye, -6, SpringLayout.NORTH, comboEye);
+		springpanel.putConstraint(SpringLayout.SOUTH, labelEye, -6, SpringLayout.NORTH, comboEye);
 		springpanel.putConstraint(SpringLayout.NORTH, comboEye, 238, SpringLayout.NORTH, this);
-		springpanel.putConstraint(SpringLayout.WEST, comboEye, 0, SpringLayout.WEST, lblEmoState);
+		springpanel.putConstraint(SpringLayout.WEST, comboEye, 0, SpringLayout.WEST, labelEmoState);
 		springpanel.putConstraint(SpringLayout.EAST, comboEye, 0, SpringLayout.EAST, comboUpperFace);
 		
-		springpanel.putConstraint(SpringLayout.NORTH, rdbtnActive, -1, SpringLayout.NORTH, comboEye);
-		springpanel.putConstraint(SpringLayout.WEST, rdbtnActive, 120, SpringLayout.WEST, this);
-		springpanel.putConstraint(SpringLayout.EAST, rdbtnActive, 0, SpringLayout.EAST, timeElapsedTextbox);
+		springpanel.putConstraint(SpringLayout.NORTH, radiobuttonActive, -1, SpringLayout.NORTH, comboEye);
+		springpanel.putConstraint(SpringLayout.WEST, radiobuttonActive, 120, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.EAST, radiobuttonActive, 0, SpringLayout.EAST, timeElapsedTextbox);
 		
-		springpanel.putConstraint(SpringLayout.NORTH, chckbxEyeAutoReset, 6, SpringLayout.SOUTH, comboEye);
-		springpanel.putConstraint(SpringLayout.WEST, chckbxEyeAutoReset, 0, SpringLayout.WEST, lblEmoState);
-		springpanel.putConstraint(SpringLayout.EAST, chckbxEyeAutoReset, -359, SpringLayout.EAST, this);
+		springpanel.putConstraint(SpringLayout.NORTH, checkboxEyeAutoReset, 6, SpringLayout.SOUTH, comboEye);
+		springpanel.putConstraint(SpringLayout.WEST, checkboxEyeAutoReset, 0, SpringLayout.WEST, labelEmoState);
+		springpanel.putConstraint(SpringLayout.EAST, checkboxEyeAutoReset, -359, SpringLayout.EAST, this);
 		
-		springpanel.putConstraint(SpringLayout.NORTH, lblEmoengineLogs, 316, SpringLayout.NORTH, this);
-		springpanel.putConstraint(SpringLayout.WEST, lblEmoengineLogs, 25, SpringLayout.WEST, this);
-		springpanel.putConstraint(SpringLayout.EAST, lblEmoengineLogs, 124, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.NORTH, labelEmoState, 316, SpringLayout.NORTH, this);
+		springpanel.putConstraint(SpringLayout.WEST, labelEmoState, 25, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.EAST, labelEmoState, 124, SpringLayout.WEST, this);
 		
 		springpanel.putConstraint(SpringLayout.NORTH, scroll, 345, SpringLayout.NORTH, this);
 		springpanel.putConstraint(SpringLayout.WEST, scroll, 22, SpringLayout.WEST, this);
 		springpanel.putConstraint(SpringLayout.SOUTH, scroll, 474, SpringLayout.NORTH, this);
 		springpanel.putConstraint(SpringLayout.EAST, scroll, 458, SpringLayout.WEST, this);
 		
-		springpanel.putConstraint(SpringLayout.NORTH, btnClearLogs, 487, SpringLayout.NORTH, this);
-		springpanel.putConstraint(SpringLayout.WEST, btnClearLogs, 185, SpringLayout.WEST, this);
-		springpanel.putConstraint(SpringLayout.EAST, btnClearLogs, 282, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.NORTH, buttonClearLogs, 487, SpringLayout.NORTH, this);
+		springpanel.putConstraint(SpringLayout.WEST, buttonClearLogs, 185, SpringLayout.WEST, this);
+		springpanel.putConstraint(SpringLayout.EAST, buttonClearLogs, 282, SpringLayout.WEST, this);
 		
-		springpanel.putConstraint(SpringLayout.NORTH, lblAffective, 0, SpringLayout.NORTH, lblEye);
-		springpanel.putConstraint(SpringLayout.WEST, lblAffective, 0, SpringLayout.WEST, lblLowerface);
+		springpanel.putConstraint(SpringLayout.NORTH, labelAffective, 0, SpringLayout.NORTH, labelEye);
+		springpanel.putConstraint(SpringLayout.WEST, labelAffective, 0, SpringLayout.WEST, labelLowerface);
 		
 		springpanel.putConstraint(SpringLayout.WEST, comboLowerFace, 0, SpringLayout.WEST, comboAffective);
-		springpanel.putConstraint(SpringLayout.WEST, comboAffective, 47, SpringLayout.EAST, rdbtnActive);
+		springpanel.putConstraint(SpringLayout.WEST, comboAffective, 47, SpringLayout.EAST, radiobuttonActive);
 		springpanel.putConstraint(SpringLayout.NORTH, comboAffective, 238, SpringLayout.NORTH, this);
 		
 		springpanel.putConstraint(SpringLayout.EAST, comboAffective, -10, SpringLayout.WEST, spinnerAffective);
-		springpanel.putConstraint(SpringLayout.NORTH, spinnerAffective, 0, SpringLayout.NORTH, rdbtnActive);
+		springpanel.putConstraint(SpringLayout.NORTH, spinnerAffective, 0, SpringLayout.NORTH, radiobuttonActive);
 		springpanel.putConstraint(SpringLayout.WEST, spinnerAffective, 0, SpringLayout.WEST, spinnerLowerFace);
 		springpanel.putConstraint(SpringLayout.EAST, spinnerAffective, 0, SpringLayout.EAST, spinnerLowerFace);
 		
 	}
+	
+	/*
+	 * Adds components to server
+	 */
 
 	public void addComponent() {
 		
-		add(lblEmoState);
+		add(labelEmoState);
 		add(spinnerAffective);
 		add(comboAffective);
-		add(lblAffective);
-		add(btnClearLogs);
+		add(labelAffective);
+		add(buttonClearLogs);
 		add(scroll);
-		add(lblEmoengineLogs);
-		add(chckbxEyeAutoReset);
-		add(rdbtnActive);
+		add(labelEmoengineLogs);
+		add(checkboxEyeAutoReset);
+		add(radiobuttonActive);
 		add(comboEye);
-		add(lblEye);
+		add(labelEye);
 		add(spinnerLowerFace);
 		add(comboLowerFace);
-		add(lblLowerface);
+		add(labelLowerface);
 		add(spinnerUpperFace);
 		add(comboUpperFace);
-		add(lblUpperFace);
-		add(lblSeconds);
+		add(labelUpperFace);
+		add(labelSeconds);
 		add(timeElapsedTextbox);
-		add(lblTime);
+		add(labelTime);
 	}
 
+	/**
+	 * 
+	 * @return detection control object
+	 */
 	public DetectionController getDetectionController() {
 		return this.detectionController;
 	}
